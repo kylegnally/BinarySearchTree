e9eacfcb0cc97008dda908de2ba6d895c02b131b
occulus1975 <occulus1975@gmail.com>
2020-05-22 17:21:15 -0400

    Copy Node and Tree class code from example.

--- BinarySearchTree/Node.cs
+++ BinarySearchTree/Node.cs
@@ -1,7 +1,63 @@
-﻿namespace BinarySearchTree
+﻿using System;
+
+
+namespace BinarySearchTree
 {
-    class Node
+    public class Node<T>
     {
-        // node class here
+        private T el;
+        private Node<T> left;
+        private Node<T> right;
+
+        public Node(T element)
+        {
+            el = element;
+            left = null;
+            right = null;
+        }
+
+        public T Element
+        {
+            get
+            {
+                return el;
+            }
+            set
+            {
+                el = value;
+            }
+        }
+
+        public Node<T> Left
+        {
+            get
+            {
+                return left;
+            }
+            set
+            {
+                left = value;
+            }
+        }
+
+        public Node<T> Right
+        {
+            get
+            {
+                return right;
+            }
+            set
+            {
+                right = value;
+            }
+        }
+
+        public Boolean isLeaf
+        {
+            get
+            {
+                return left == null && right == null;
+            }
+        }
     }
 }
--- BinarySearchTree/Tree.cs
+++ BinarySearchTree/Tree.cs
@@ -1,7 +1,238 @@
-﻿namespace BinarySearchTree
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
 {
-    class Tree
+    public enum Relative
     {
-        //tree class here
+        LeftChild, RightChild, Parent, Root
+    }
+
+    public class Tree<T>
+    {
+
+
+        private Node<T> root;
+        private Node<T> current;
+        private int size;
+
+        public Tree()
+        {
+            root = null;
+            current = null;
+            size = 0;
+        }
+
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+        }
+
+        public Node<T> Current
+        {
+            get
+            {
+                return current;
+            }
+        }
+
+        public Node<T> Root
+        {
+            get
+            {
+                return root;
+            }
+        }
+
+        public Boolean isEmpty
+        {
+            get
+            {
+                return root == null;
+            }
+        }
+
+        public Boolean moveTo(Relative relative)
+        {
+            Boolean found = false;
+
+
+            switch (relative)
+            {
+                case Relative.LeftChild:
+                    if (current.Left != null)
+                    {
+                        current = current.Left;
+                        found = true;
+                    }
+
+                    break;
+
+                case Relative.RightChild:
+                    if (current.Right != null)
+                    {
+                        current = current.Right;
+                        found = true;
+                    }
+                    break;
+
+                case Relative.Root:
+                    if (root != null)
+                    {
+                        current = root;
+                        found = true;
+                    }
+                    break;
+
+                case Relative.Parent:
+                    if (current != root)
+                    {
+                        current = findParent(current);
+                        found = true;
+                    }
+                    break;
+            }
+
+            return found;
+        }
+
+        public Node<T> findParent(Node<T> node)
+        {
+            Stack<Node<T>> s = new Stack<Node<T>>();
+            Node<T> n = root;
+
+            while (n.Left != node && n.Right != node)
+            {
+                if (n.Right != null)
+                    s.Push(n.Right);
+
+                if (n.Left != null)
+                    n = n.Left;
+                else
+                    n = s.Pop();
+            }
+            s.Clear();
+            return n;
+        }
+
+        public Boolean Insert(T value, Relative relative)
+        {
+            Boolean inserted = true;
+            Node<T> newNode = new Node<T>(value);
+
+            if ((relative == Relative.LeftChild && current.Left != null) ||
+                  relative == Relative.RightChild && current.Right != null)
+            {
+                inserted = false;
+            }
+            else
+            {
+                if (relative == Relative.LeftChild)
+                {
+                    current.Left = newNode;
+                }
+                else if (relative == Relative.RightChild)
+                {
+                    current.Right = newNode;
+                }
+                else if (relative == Relative.Root)
+                {
+                    if (root == null)
+                    {
+                        root = newNode;
+                    }
+                    current = root;
+                }
+            }
+
+            if (inserted)
+                size++;
+
+            return inserted;
+        }
+
+        public Boolean Insert(Node<T> treeNode, Relative relative)
+        {
+            Boolean inserted = true;
+            Node<T> newNode = new Node<T>(treeNode.Element);
+
+            if ((relative == Relative.LeftChild && current.Left != null) ||
+                  relative == Relative.RightChild && current.Right != null)
+            {
+                inserted = false;
+            }
+            else
+            {
+                if (relative == Relative.LeftChild)
+                {
+                    current.Left = newNode;
+                }
+                else if (relative == Relative.RightChild)
+                {
+                    current.Right = newNode;
+                }
+                else if (relative == Relative.Root)
+                {
+                    if (root == null)
+                    {
+                        root = newNode;
+                    }
+                    current = root;
+                }
+            }
+
+            if (inserted)
+                size++;
+
+            return inserted;
+        }
+
+
+        public void Destroy(Node<T> node)
+        {
+            if (node != null)
+            {
+                Destroy(node.Left);
+                Destroy(node.Right);
+                node = null;
+                size--;
+            }
+        }
+
+        public void InOrder(Node<T> node)
+        {
+            if (node != null)
+            {
+                InOrder(node.Left);
+                Console.Write(node.Element.ToString());
+                InOrder(node.Right);
+            }
+        }
+
+        public void PreOrder(Node<T> node)
+        {
+            if (node != null)
+            {
+                Console.Write(node.Element.ToString());
+                PreOrder(node.Left);
+                PreOrder(node.Right);
+            }
+        }
+
+        public void PostOrder(Node<T> node)
+        {
+            if (node != null)
+            {
+                PostOrder(node.Left);
+                PostOrder(node.Right);
+                Console.Write(node.Element.ToString());
+            }
+        }
     }
 }
